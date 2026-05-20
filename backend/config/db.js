f28c6a06b00e73b51bd1c8767d112c7390c07b42
Rohit Kumar <kumarrohitsingh650@gmail.com>
2026-05-20 11:59:38 +0530

    Updated website and fixed latest changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,16 @@
 const { Sequelize } = require('sequelize');
-const path = require('path');
-require('dotenv').config();
+require('dotenv').config({ path: require('path').join(__dirname, '..', '.env') });
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: path.join(__dirname, '..', 'database.sqlite'),
-  logging: false
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'mysql',
+    logging: false,
+    port: process.env.DB_PORT || 3306
+  }
+);
 
 module.exports = sequelize;
-
